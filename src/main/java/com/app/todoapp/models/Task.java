2f104ee96d9af04b9f473c03ca5c3fa1cb9cfd5d
topograp2 <topo.or.dubu@gmail.com>
2024-12-31 17:08:00 +0900

    done

---
+++ src/main/java/com/app/todoapp/models/Task.java
@@ -0,0 +1,44 @@
+package com.app.todoapp.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    private boolean completed;
+
+}
